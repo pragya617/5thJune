ea864df1d64ca5809dd5b45bd983b4e019c9bf81
pragya617 <79211225+pragya617@users.noreply.github.com>
2021-03-22 13:00:11 +0530

    Update someapp.py

--- someapp.py
+++ someapp.py
@@ -2,7 +2,7 @@ print("first line")
 print("second line")
 print("line for second commit")
 name = "pragya"
-print("hello" + pragya)
+print("hello" + name)
 
 print("code added by 2nd developer")
 print("line added by 1st developer")
